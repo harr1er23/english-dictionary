62cbaae28c1f31d4ba052f226be7c0545533aa2f
harr1er23 <harr1er.work23@gmail.com>
2023-10-17 19:45:52 +0300

    Added tag's input validation

--- src/components/ModalAddWord/index.jsx
+++ src/components/ModalAddWord/index.jsx
@@ -24,7 +24,6 @@ const ModalAddWord = ({
   tagsValue = [],
   examplesValue = [],
 }) => {
-
   //контроллер состояния инпута ввода слова
   const [word, setWord] = React.useState(wordValue);
 
@@ -122,31 +121,7 @@ const ModalAddWord = ({
   };
 
   const addTagInBd = async (arrayMethod, variable, clearInputMethod, array) => {
-    if (
-      (array.length !== 0 &&
-        array.find(
-          (obj) => obj.value.toLowerCase() === variable.value.toLowerCase()
-        )) ||
-      (tags.length !== 0 &&
-        tags.find(
-          (obj) => obj.value.toLowerCase() === variable.value.toLowerCase()
-        ))
-    ) {
-      clearInputMethod("");
-    } else {
-      await axios.post(process.env.REACT_APP_TAGS_KEY, variable);
-      addVariableToArr(arrayMethod, variable, clearInputMethod, array);
-    }
-  };
-
-  const addTagInBdOnEnter = async (
-    event,
-    arrayMethod,
-    variable,
-    clearInputMethod,
-    array
-  ) => {
-    if (event.key === "Enter") {
+    if (variable.value.length !== 0) {
       if (
         (array.length !== 0 &&
           array.find(
@@ -158,9 +133,43 @@ const ModalAddWord = ({
           ))
       ) {
         clearInputMethod("");
+        toast.error("Такой тег уже существует!");
       } else {
         await axios.post(process.env.REACT_APP_TAGS_KEY, variable);
         addVariableToArr(arrayMethod, variable, clearInputMethod, array);
+      }
+    } else {
+      toast.error("Заполните поле!");
+    }
+  };
+
+  const addTagInBdOnEnter = async (
+    event,
+    arrayMethod,
+    variable,
+    clearInputMethod,
+    array
+  ) => {
+    if (event.key === "Enter") {
+      if (variable.value.length !== 0) {
+        if (
+          (array.length !== 0 &&
+            array.find(
+              (obj) => obj.value.toLowerCase() === variable.value.toLowerCase()
+            )) ||
+          (tags.length !== 0 &&
+            tags.find(
+              (obj) => obj.value.toLowerCase() === variable.value.toLowerCase()
+            ))
+        ) {
+          clearInputMethod("");
+          toast.error("Такой тег уже существует!");
+        } else {
+          await axios.post(process.env.REACT_APP_TAGS_KEY, variable);
+          addVariableToArr(arrayMethod, variable, clearInputMethod, array);
+        }
+      } else {
+        toast.error("Заполните поле!");
       }
     }
   };
@@ -310,25 +319,25 @@ const ModalAddWord = ({
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                   <g
                     id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></g>
                   <g id="SVGRepo_iconCarrier">
                     {" "}
                     <path
                       d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                     <path
                       d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                   </g>
                 </svg>
@@ -403,25 +412,25 @@ const ModalAddWord = ({
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                   <g
                     id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></g>
                   <g id="SVGRepo_iconCarrier">
                     {" "}
                     <path
                       d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                     <path
                       d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                   </g>
                 </svg>
@@ -460,25 +469,25 @@ const ModalAddWord = ({
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
                 >
-                  <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                  <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                   <g
                     id="SVGRepo_tracerCarrier"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
                   ></g>
                   <g id="SVGRepo_iconCarrier">
                     {" "}
                     <path
                       d="M15 12L12 12M12 12L9 12M12 12L12 9M12 12L12 15"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                     <path
                       d="M22 12C22 16.714 22 19.0711 20.5355 20.5355C19.0711 22 16.714 22 12 22C7.28595 22 4.92893 22 3.46447 20.5355C2 19.0711 2 16.714 2 12C2 7.28595 2 4.92893 3.46447 3.46447C4.92893 2 7.28595 2 12 2C16.714 2 19.0711 2 20.5355 3.46447C21.5093 4.43821 21.8356 5.80655 21.9449 8"
                       stroke="#fff"
-                      stroke-width="1.5"
-                      stroke-linecap="round"
+                      strokeWidth="1.5"
+                      strokeLinecap="round"
                     ></path>{" "}
                   </g>
                 </svg>
@@ -498,26 +507,26 @@ const ModalAddWord = ({
                       fill="none"
                       xmlns="http://www.w3.org/2000/svg"
                     >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                       <g
                         id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></g>
                       <g id="SVGRepo_iconCarrier">
                         <path
                           d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
                           stroke="#fff"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         ></path>
                         <path
                           d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
                           stroke="#fff"
-                          stroke-width="1.5"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
+                          strokeWidth="1.5"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
                         ></path>
                       </g>
                     </svg>
@@ -534,11 +543,11 @@ const ModalAddWord = ({
                       xmlns="http://www.w3.org/2000/svg"
                       stroke="#ffffff"
                     >
-                      <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+                      <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
                       <g
                         id="SVGRepo_tracerCarrier"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       ></g>
                       <g id="SVGRepo_iconCarrier">
                         <path
--- src/components/Word/index.jsx
+++ src/components/Word/index.jsx
@@ -56,9 +56,9 @@ const Word = ({ word, transcription, translate, selectTagArr, learPercent}) => {
         <div className={styles.translateBlock}>
           {translate.map((obj, index) =>
             index === translate.length - 1 ? (
-              <div>{obj}</div>
+              <div key={index}>{obj}</div>
             ) : (
-              <div>{obj},</div>
+              <div key={index}>{obj},</div>
             )
           )}
         </div>
@@ -71,52 +71,52 @@ const Word = ({ word, transcription, translate, selectTagArr, learPercent}) => {
       <Circle className={styles.percentCircle} percent={learPercent} strokeWidth="10" trailWidth="7" strokeColor={learPercent <= 25 ? "#bc2b2b" : learPercent <= 50 ? "#bc872b" : learPercent <= 75 ? "#bcb72b" : "#2bbc99"} />
       <div className={styles.editImg}>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             <path
               d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
               stroke="var(--tertiary-bg)"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
             <path
               d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
               stroke="var(--tertiary-bg)"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
           </g>
         </svg>
       </div>
       <div className={styles.editImg}>
         <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
+          <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
           <g
             id="SVGRepo_tracerCarrier"
-            stroke-linecap="round"
-            stroke-linejoin="round"
+            strokeLinecap="round"
+            strokeLinejoin="round"
           ></g>
           <g id="SVGRepo_iconCarrier">
             <path
               d="M21.2799 6.40005L11.7399 15.94C10.7899 16.89 7.96987 17.33 7.33987 16.7C6.70987 16.07 7.13987 13.25 8.08987 12.3L17.6399 2.75002C17.8754 2.49308 18.1605 2.28654 18.4781 2.14284C18.7956 1.99914 19.139 1.92124 19.4875 1.9139C19.8359 1.90657 20.1823 1.96991 20.5056 2.10012C20.8289 2.23033 21.1225 2.42473 21.3686 2.67153C21.6147 2.91833 21.8083 3.21243 21.9376 3.53609C22.0669 3.85976 22.1294 4.20626 22.1211 4.55471C22.1128 4.90316 22.0339 5.24635 21.8894 5.5635C21.7448 5.88065 21.5375 6.16524 21.2799 6.40005V6.40005Z"
               stroke="var(--tertiary-bg)"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
             <path
               d="M11 4H6C4.93913 4 3.92178 4.42142 3.17163 5.17157C2.42149 5.92172 2 6.93913 2 8V18C2 19.0609 2.42149 20.0783 3.17163 20.8284C3.92178 21.5786 4.93913 22 6 22H17C19.21 22 20 20.2 20 18V13"
               stroke="var(--tertiary-bg)"
-              stroke-width="1.5"
-              stroke-linecap="round"
-              stroke-linejoin="round"
+              strokeWidth="1.5"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             ></path>
           </g>
         </svg>
